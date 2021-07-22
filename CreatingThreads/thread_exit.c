1c8ce0f4d05025632b1b920f09bf32932e4c5ff7
veena1306 <66565407+veena1306@users.noreply.github.com>
2021-07-22 14:20:14 +0530

    Create thread_exit.c

---
+++ CreatingThreads/thread_exit.c
@@ -0,0 +1,42 @@
++++++++++ writing a hello world program ++++++++++
+
+#include<pthread.h>
+  
+Static void* thread_fn_callback(void *arg){
+ char *input = (char*)arg;
+ int a;
+  while(a > 10) 
+  {
+   printf(“input string is %s\n”, input);
+   sleep(1);
+   if(a == 5){
+    pthread_exit(0);
+   }
+  }
+} 
+  
+void thread_create()
+{
+
+  pthread_t pthread1;                                   // creating the threads, thread handle(pthread1)
+  
+  static char *thread_input = "i am into thread1";      // (input to the thread, thats goint to create) 
+  
+  int rc = pthread_create(&pthread1, NULL, thread_fn_callback,(void*)thread_input);    // 1st arg -  address of thread handle //3rd - pointer to the functionn
+   if(rc != 0)                                                                        // address of the memory going to pass input to the child thread
+   {
+    printf("error occured, while thread creation %d", rc) 
+     exit();
+   }
+  
+                                                                                       
+}                                                                                       
+
+int main(int argc, char **argv)
+{ 
+  thread_create();
+  printf("main is paused");
+  pause();
+  
+  return 0;
+}
